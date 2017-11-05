ca12bb0cfb5bb10b0a95fe71ba0178df300b029c
Austin Gresham <austin.gresham@roundtower.com>
2017-11-05 18:28:06 -0500

    Added array.js

---
+++ array.js
@@ -0,0 +1,13 @@
+let NewYearsResolutions = ['Rappel into a cave', 'Take a falconry class', 'Learn to Juggle'];
+console.log(NewYearsResolutions[0]);
+console.log(NewYearsResolutions[1]);
+console.log(NewYearsResolutions[2]);
+console.log(NewYearsResolutions);
+
+let hello = 'Hello World';
+console.log(hello[6]);
+
+let firstWord = NewYearsResolutions[0];
+console.log(firstWord);
+
+console.log(NewYearsResolutions[3]);
