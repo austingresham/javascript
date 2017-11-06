6ef5f09757c94887a8ad1dd20df70df6fdc7cb07
Austin Gresham <austin.gresham@roundtower.com>
2017-11-05 19:15:18 -0500

    Changed array.js

--- array.js
+++ array.js
@@ -4,10 +4,20 @@ console.log(NewYearsResolutions[1]);
 console.log(NewYearsResolutions[2]);
 console.log(NewYearsResolutions);
 
+// Strings are stored as an array
 let hello = 'Hello World';
 console.log(hello[6]);
 
+// Set a value based on an array member value
 let firstWord = NewYearsResolutions[0];
 console.log(firstWord);
 
-console.log(NewYearsResolutions[3]);
+console.log(NewYearsResolutions[3]);  //undefined
+
+let seasons = ['Winter', 'Spring', 'Summer', 'Fall'];
+console.log(seasons);
+
+//modify an array member
+seasons[0] = 'Cold!';
+console.log(seasons);
+
